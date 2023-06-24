db6f4479832424d0af0de125a14c6bcf038a69b5
RexOseiOwusu <roseiowusu6@gmail.com>
2023-06-24 22:22:49 +0200

    Loading and plotting the Mauna Loa csv file

---
+++ Mauna Co2.R
@@ -0,0 +1,7 @@
+# Opening the Mauna Loa csv file
+co2 = read.csv("co2_mm_mlo.csv")
+
+# Plotting the data
+plot(co2$decdate, co2$interpolated, type='l', main="Mauna Loa CO2",
+     xlab="Time", ylab="ppm")
+lines(co2$decdate, co2$trend, col=2, lwd=2)
