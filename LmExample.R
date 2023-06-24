777bfa549638f945e92bce8545139242b78ab2b2
RexOseiOwusu <roseiowusu6@gmail.com>
2023-06-24 22:12:26 +0200

    Plotting the data and linear model

--- LmExample.R
+++ LmExample.R
@@ -1,6 +1,14 @@
+# Creating a data of random numbers with the mean and standard deviation specified
 x = rnorm(100, mean = 20, sd = 5)
 y = x + rnorm(100, mean = 0, sd = 2)
 
+# Building a linear model for the data
 fit = lm(y ~ x)
 
+# Summary of the linear model
 summary(fit)
+
+# Plotting the data and the model fit 
+plot(x,y)
+abline(fit, col=2)
+
